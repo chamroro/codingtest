6cebb4d938ec983a2c063a810b85febcee3f7bf2
chamroro <22000197@handong.edu>
2022-07-06 15:40:51 +0900

    https://programmers.co.kr/learn/courses/30/lessons/12910

---
+++ programmers/4.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] arr, int divisor) {
+        int[] answer = {};
+        ArrayList<Integer> list = new ArrayList<Integer>();
+    
+        for(int i=0; i<arr.length; i++){
+            if(arr[i] % divisor == 0){
+                list.add(arr[i]);
+            }
+        }
+       
+        answer = new int[list.size()];
+        
+		for(int i=0; i < list.size(); i++) {
+	   		answer[i] = list.get(i);
+    	}
+        
+	    Arrays.sort(answer);
+	    if(list.size()==0){ 
+            answer = new int[1];
+            answer[0]= -1;
+        }
+        return answer;
+    }
+}
