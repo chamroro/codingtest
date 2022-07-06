7fb9b28606f50e02793cfa133315019046708928
chamroro <22000197@handong.edu>
2022-07-06 16:03:57 +0900

    https://school.programmers.co.kr/learn/courses/30/lessons/12916?language=java

---
+++ programmers/6.java
@@ -0,0 +1,16 @@
+class Solution {
+    boolean solution(String s) {
+        boolean answer = true;
+        String[] arr= s.split("");
+        int p=0;
+        int y=0;
+        for(int i=0; i < arr.length; i++){
+            if(arr[i].equals("P")||arr[i].equals("p"))
+                p++;
+            else if (arr[i].equals("Y") || arr[i].equals("y"))
+                y++;
+        }
+        if(p!=y) answer = false;
+        return answer;
+    }
+}
