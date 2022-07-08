054c9f3af33aec425251af3c4414c3f2a216a0a4
chamroro <22000197@handong.edu>
2022-07-09 01:29:12 +0900

    https://www.acmicpc.net/problem/2475

---
+++ beakjoon/15.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        int theNumber=0;
+        Scanner sc = new Scanner(System.in);
+        for(int i=0; i<5; i++){
+            theNumber+= Math.pow(sc.nextInt(),2);
+        }
+        theNumber = theNumber %10;
+        sc.close();
+        System.out.print(theNumber);
+
+    }
+}
