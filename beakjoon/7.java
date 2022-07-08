569723ad0ed8134a6fd3b3d81d89aae5b744906d
chamroro <22000197@handong.edu>
2022-07-08 15:13:38 +0900

    https://www.acmicpc.net/problem/2920

---
+++ beakjoon/7.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] scales = new int[8];
+        String result;
+        for(int i=0; i<8; i++){
+            scales[i]= sc.nextInt();
+        }
+        int flag=0;
+        if(scales[0]==1){
+            for(int i=0; i<7; i++){
+                if(scales[i]>scales[i+1]){
+                    flag=1;
+                    break;
+                }
+            }
+            if(flag==0) result="ascending"; 
+            else result="mixed";
+        } else if (scales[0]==8){
+            for(int i=0; i<7; i++){
+                if(scales[i]<scales[i+1]){
+                    flag=1;
+                    break;
+                }
+            }
+            if(flag==0) result="descending"; 
+            else result="mixed";
+        } else result="mixed";
+
+        System.out.println(result);
+    }
+}
