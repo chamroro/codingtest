b6c23610e22035529ac7f6401932264846e2b9fb
chamroro <22000197@handong.edu>
2022-07-09 23:24:07 +0900

    https://www.acmicpc.net/problem/5585

---
+++ week1/A110.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+public class Main {
+
+
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+        int input=sc.nextInt();
+        int money=1000-input;
+        int result=0;
+        int count=0;
+        if(money>=500){
+            count=money/500;
+            result+= count;
+            money-=count*500;
+        }
+        if(money>=100){
+            count=money/100;
+            result+= count;
+            money-=count*100;
+        }
+        if(money>=50){
+            count=money/50;
+            result+= count;
+            money-=count*50;
+        }
+        if(money>=10){
+            count=money/10;
+            result+= count;
+            money-=count*10;
+        }
+        if(money>=5){
+            count=money/5;
+            result+= count;
+            money-=count*5;
+        }
+        if(money>=1){
+            result+= money;
+        }
+        sc.close();
+        System.out.println(result);
+
+    }
+
+ 
+}
