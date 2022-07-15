c3ec5b3ce1bc4ebe6ba162c2554e39960f8b5d4a
chamroro <22000197@handong.edu>
2022-07-15 23:09:40 +0900

    https://leetcode.com/problems/determine-if-string-halves-are-alike/

---
+++ week2/A040_김하은_20220715.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean halvesAreAlike(String s) {
+        String[] aString=s.split("");
+  
+        int a=0, b=0;
+        for(int i=0; i<aString.length; i++){
+            
+            if(aString[i]=="a"||aString[i]=="e"||aString[i]=="i"||aString[i]=="o"||aString[i]=="u"){
+                if(i<aString.length/2) a++;
+                else b++;
+            }
+        }
+        if(a==b) return true;
+        else return false;
+    }
+
+}
