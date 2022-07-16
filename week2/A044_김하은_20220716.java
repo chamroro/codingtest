c7d4b83679b0c5531a47c4ff108824befd86e62d
chamroro <22000197@handong.edu>
2022-07-16 16:43:28 +0900

    https://leetcode.com/problems/student-attendance-record-i/

---
+++ week2/A044_김하은_20220716.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean checkRecord(String s) {
+        int a=0;
+        for(int i=0; i<s.length(); i++){
+            if(s.charAt(i)=='A') a++;
+        }
+        if(a>=2 || s.contains("LLL")) return false;
+        else return true;
+    }
+}
