1575c050664a968c42586779d9bb57a2a3708165
chamroro <chamsae@handong.ac.kr>
2022-07-17 16:15:13 +0900

    https://www.acmicpc.net/problem/1316

---
+++ week2/A048_김하은_20220717.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int rp= sc.nextInt();
+        int count=0;
+        for(int i=0; i<rp; i++){
+            int flag=0;
+            String[] abc= sc.next().split("");
+            for(int j=0; j<abc.length; j++){
+                for(int k=0; k<j; k++){
+                    if(abc[j].equals(abc[k])&&!abc[j].equals(abc[j-1])){
+                        count++;
+                        flag=1;
+                        break;
+                    }
+                }
+                if(flag==1) break;
+            }
+        }
+        System.out.println(rp-count);
+        sc.close();
+    }
+}
